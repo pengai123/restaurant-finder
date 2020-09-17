48cfa21d3efd40a629f02cfc140a1a4f444f66b9
Peng Ai <aaaipeng@gmail.com>
2020-09-16 23:52:22 -0700

    update for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bp = require("body-parser")
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const axios = require("axios");
 //const config = require("./config.js")
 
